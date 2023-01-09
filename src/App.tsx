ab5014fd2ac7e3b1b4535a427fdb71176148cc93
elias <elias.e.solis@gmail.com>
2023-01-09 17:13:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Contact from "./routes/Contact";
+
+import { Header } from "./layout/Header";
+import { Main } from "./layout/Main";
+
+import { WavyContainer } from "./library/waves";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <WavyContainer />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+
+              <Main />
+            </>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route
+            path="about"
+            element={
+              <Suspense fallback={<>...</>}>
+                <About />
+              </Suspense>
+            }
+          />
+          <Route
+            path="contact"
+            element={
+              <Suspense fallback={<>...</>}>
+                <Contact />
+              </Suspense>
+            }
+          />
+          <Route path="*" element={<>No Match</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
